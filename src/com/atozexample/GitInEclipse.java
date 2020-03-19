13d80e125aec1c3e0124096597aaeb116c85ab7b
LeAnhdung <jlehoola@yahoo.com>
2020-03-19 14:52:56 -0700

    Add comment  in git* file

--- src/com/atozexample/GitInEclipse.java
+++ src/com/atozexample/GitInEclipse.java
@@ -5,7 +5,7 @@ public class GitInEclipse {
 	public GitInEclipse() {
 		// TODO Auto-generated constructor stub
 	}
-
+// this is the main method
 	public static void main(String[] args) {
 		System.out.println("This is the 1st  gi clone prject in Eclipse");
 
